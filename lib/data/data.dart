e94ca4e254a128ef9f9133478face12dea2599a3
Leandre <leajapo@gmail.com>
2020-11-13 20:08:19 +0200

    feat:added produts dummy data

---
+++ lib/data/data.dart
@@ -0,0 +1,39 @@
+class Product {
+  int id;
+  String name;
+  String description;
+  String image;
+  double price;
+
+  Product({
+    this.id,
+    this.description,
+    this.name,
+    this.image,
+    this.price,
+  });
+}
+
+List<Product> products = [
+  Product(
+    id: 1,
+    name: 'Burger',
+    description: 'THis is a burger',
+    image: 'assets/images/burger.png',
+    price: 12,
+  ),
+  Product(
+    id: 2,
+    name: 'Burger',
+    description: 'THis is a burger',
+    image: 'assets/images/burger.png',
+    price: 12,
+  ),
+  Product(
+    id: 3,
+    name: 'Burger',
+    description: 'THis is a burger',
+    image: 'assets/images/burger.png',
+    price: 12,
+  ),
+];
