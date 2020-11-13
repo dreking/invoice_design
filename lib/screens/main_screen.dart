2f2dfe63dc9e6875ed59e7373cb582bc90123746
Leandre <leajapo@gmail.com>
2020-11-13 20:07:32 +0200

    feat:added main screen design

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:invoice_design/components/add_cart_button.dart';
+import 'package:invoice_design/components/all_products.dart';
+import 'package:invoice_design/components/custom_header.dart';
+import 'package:invoice_design/components/download_button.dart';
+
+class MainScreen extends StatelessWidget {
+  static const routeName = '/';
+  const MainScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomHeader(size: size),
+            ItemsWidget(),
+            ProductsWidget(size: size),
+            CustomButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
