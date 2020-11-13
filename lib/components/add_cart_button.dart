3ce74326af79f768a91844579fbabe366ca793b1
Leandre <leajapo@gmail.com>
2020-11-13 20:09:29 +0200

    feat:added add to cart button components

---
+++ lib/components/add_cart_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ItemsWidget extends StatelessWidget {
+  const ItemsWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        children: [
+          Text('Items:'),
+          SizedBox(width: 20),
+          FlatButton.icon(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            color: Colors.orange[100],
+            icon: Icon(Icons.add),
+            label: Text('Add to cart'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
