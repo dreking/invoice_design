c8ed6d824072a79f13e1da77fffb8b55ba1c5f55
Leandre <leajapo@gmail.com>
2020-11-13 20:10:28 +0200

    feat:added download button

---
+++ lib/components/download_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width * 0.9,
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: RaisedButton.icon(
+        padding: EdgeInsets.symmetric(vertical: 15),
+        icon: Icon(Icons.download_sharp),
+        label: Text('Download'),
+        color: Colors.orange[300],
+        onPressed: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
